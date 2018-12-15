c82e2a801a034893c9f4720c7aed263344635e08
Unknown <jzsab1@gmail.com>
2018-12-15 11:01:00 +0100

    It Compiles

--- rsa/src/rsa.cpp
+++ rsa/src/rsa.cpp
@@ -20,26 +20,28 @@ using namespace std;
 //char msg[100];
 
 class Rsa {
-	int primeNumber;
-	int anotherPrimeNumber;
-	int t, n;
-	long int temp[50];
-	std::string message = "";
-	std::string nameOfClient = "";
-	char msg[255];
-	long int publicKey[50], privateKey[50];
+	std::string nameOfClient, message;
+	int primeNumber, anotherPrimeNumber, t, n;
+	long int temp[50], publicKey[50], privateKey[50];
+	char msg[255], enMsg[255];
 
 public:
 	Rsa(std::string _nameOfClient, std::string _message, int _primeNumber, int _anotherPrimeNumber) :
 	nameOfClient(_nameOfClient), message(_message), primeNumber(_primeNumber), anotherPrimeNumber(_anotherPrimeNumber) {
 		t = (primeNumber - 1) * (anotherPrimeNumber - 1);
 		n = primeNumber * anotherPrimeNumber;
+		convertMessage();
+	}
+
+	void convertMessage() {
+		strncpy(msg, message.c_str(), sizeof(msg));
+		msg[sizeof(msg) - 1] = 0;
 	}
 
 	int prime(long int pr)
 	{
 	   int j = sqrt(pr);
-	   for(unsigned int i = 2; i <= j; i++)
+	   for(int i = 2; i <= j; i++)
 	   {
 	      if(pr % i == 0)
 	         return 0;
@@ -52,7 +54,7 @@ public:
 	{
 	   int k;
 	   k = 0;
-	   for(unsigned int i = 2; i < t; i++)
+	   for(int i = 2; i < t; i++)
 	   {
 		  int flag;
 	      if(t % i == 0)
@@ -93,47 +95,47 @@ public:
 
 	   while(i != len)
 	   {
-	      pt = m[i];
+	      pt = msg[i];
 	      pt = pt - 96;
 	      k = 1;
-	      for(j = 0; j < key; j++)
+	      for(int j = 0; j < key; j++)
 	      {
 	         k = k * pt;
 	         k = k % n;
 	      }
 	      temp[i] = k;
 	      ct= k + 96;
-	      en[i] = ct;
+	      enMsg[i] = ct;
 	      i++;
 	   }
-	   en[i] = -1;
+	   enMsg[i] = -1;
 	   cout << "\n\nTHE ENCRYPTED MESSAGE IS\n";
-	   for(i=0; en[i] != -1; i++)
-	      cout << en[i];
+	   for(i=0; enMsg[i] != -1; i++)
+	      cout << enMsg[i];
 	}
 
 	//function to decrypt the message
 	void decrypt()
 	{
 	   long int pt, ct, key = privateKey[0], k;
-	   i = 0;
-	   while(en[i] != -1)
+	   int i = 0;
+	   while(enMsg[i] != -1)
 	   {
 	      ct = temp[i];
 	      k = 1;
-	      for(j = 0; j < key; j++)
+	      for(int j = 0; j < key; j++)
 	      {
 	         k = k * ct;
 	         k = k % n;
 	      }
 	      pt = k + 96;
-	      m[i] = pt;
+	      msg[i] = pt;
 	      i++;
 	   }
-	   m[i] = -1;
+	   msg[i] = -1;
 	   cout << "\n\nTHE DECRYPTED MESSAGE IS\n";
-	   for(i = 0; m[i] != -1; i++)
-	      cout << m[i];
+	   for(i = 0; msg[i] != -1; i++)
+	      cout << msg[i];
 
 	  cout << endl;
 	}
@@ -141,45 +143,45 @@ public:
 
 int main()
 {
-	Rsa rsa;
-   cout << "\nENTER FIRST PRIME NUMBER\n";
-   cin >> x;
-
-   //checking whether input is prime or not
-   flag = rsa.prime(x);
-   if(flag == 0)
-   {
-      cout << "\nINVALID INPUT\n";
-      exit(0);
-   }
-
-   cout << "\nENTER SECOND PRIME NUMBER\n";
-   cin >> y;
-
-   flag = rsa.prime(y);
-   if(flag == 0 || x == y)
-   {
-      cout << "\nINVALID INPUT\n";
-      exit(0);
-   }
-
-   cout << "\nENTER MESSAGE OR STRING TO ENCRYPT\n";
-   cin >> msg;
-
-   for(i = 0; msg[i] != 0; i++)
-      m[i] = msg[i];
-   n = x * y;
-   t = (x - 1) * (y - 1);
-
-   rsa.encryption_key();
-   cout << "\nPOSSIBLE VALUES OF e AND d ARE\n";
-
-   for(i = 0; i < j - 1; i++)
-      cout << "\n" << e[i] << "\t" << d[i];
-
-   rsa.encrypt();
-   rsa.decrypt();
-   return 0;
+//	Rsa rsa;
+//   cout << "\nENTER FIRST PRIME NUMBER\n";
+//   cin >> x;
+//
+//   //checking whether input is prime or not
+//   flag = rsa.prime(x);
+//   if(flag == 0)
+//   {
+//      cout << "\nINVALID INPUT\n";
+//      exit(0);
+//   }
+//
+//   cout << "\nENTER SECOND PRIME NUMBER\n";
+//   cin >> y;
+//
+//   flag = rsa.prime(y);
+//   if(flag == 0 || x == y)
+//   {
+//      cout << "\nINVALID INPUT\n";
+//      exit(0);
+//   }
+//
+//   cout << "\nENTER MESSAGE OR STRING TO ENCRYPT\n";
+//   cin >> msg;
+//
+//   for(i = 0; msg[i] != 0; i++)
+//      m[i] = msg[i];
+//   n = x * y;
+//   t = (x - 1) * (y - 1);
+//
+//   rsa.encryption_key();
+//   cout << "\nPOSSIBLE VALUES OF e AND d ARE\n";
+//
+//   for(i = 0; i < j - 1; i++)
+//      cout << "\n" << e[i] << "\t" << d[i];
+//
+//   rsa.encrypt();
+//   rsa.decrypt();
+//   return 0;
 } //end of the main program
 
 
